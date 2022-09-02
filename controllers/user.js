af685217cb2206b28de456ff4790c74a50b3002f
Kepoulpe <moreno.n@hotmail.fr>
2022-09-02 17:32:33 +0200

    correction for login route and async it

--- controllers/user.js
+++ controllers/user.js
@@ -43,41 +43,57 @@ exports.signup = async (req, res, next) => {
 
 // controller for user login
 exports.login = async (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                res.status(401).json({
-                    data: null,
-                    msg: 'Utilisateurs non trouvé',
-                    success: false
-                })
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        res.status(401).json({
-                            data: null,
-                            msg: 'Mot de passe incorrect',
-                            success: false
-                        })
-                    }
-                    res.status(200).json({
-                        //  TODO how to make the the response payload with data
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            process.env.RANDOM_SECRET_TOKEN,
-                            { expiresIn: '24h' }
-                        ),
-                        isAdmin: user.isAdmin
-                    });
-                })
-        })
-        .catch(error => res.status(500).json({
+    let usr;
+    try {
+        usr = await User.findOne({ email: req.body.email });
+    } catch (error) {
+        res.status(500).json({
             data: null,
-            msg: error,
+            msg: 'Une erreur est servenue, merci de réessayer ultérieument',
             success: false
-        }))
+        });
+        return;
+    }
+    if (!usr) {
+        res.status(401).json({
+            data: null,
+            msg: 'Utilisateur non trouvé',
+            success: false
+        });
+        return;
+    } 
+    let isPwdValid;
+    try {
+        isPwdValid = await bcrypt.compare(req.body.password, usr.password);
+    } catch (error) {
+        res.status(500).json({
+            data: null,
+            msg: 'Une erreur est servenue, merci de réessayer ultérieument',
+            success: false
+        });
+        return;        
+    }
+    if (!isPwdValid) {
+        res.status(401).json({
+            data: null,
+            msg: 'Mot de passe erroné',
+            success: false
+        });
+        return;
+    }
+    res.status(200).json({
+        data: {
+            isAdmin: usr.isAdmin,
+            token: jwt.sign(
+                { userId: usr._id },
+                process.env.RANDOM_SECRET_TOKEN,
+                { expiresIn: '24h' }
+            ),
+            userId: usr._id
+        },
+        msg: 'login successful',
+        success: true
+    });
 };
 
 exports.delete = async (req, res) => {
